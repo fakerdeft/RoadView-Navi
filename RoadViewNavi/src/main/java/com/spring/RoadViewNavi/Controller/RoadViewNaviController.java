d41a5176eddffd2063ea63f0add0b7b315f004b9
fakerdeft <whakswp123@naver.com>
2023-01-06 01:25:27 +0900

    feat: 지도 페이지로 가는 기능 추가

--- RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Controller/RoadViewNaviController.java
+++ RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Controller/RoadViewNaviController.java
@@ -1,10 +1,15 @@
 package com.spring.RoadViewNavi.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.RoadViewNavi.Model.Service.RoadViewNaviService;
+import com.spring.RoadViewNavi.Model.VO.User;
 
 @Controller
 public class RoadViewNaviController {
@@ -14,6 +19,12 @@ public class RoadViewNaviController {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
+	@GetMapping("map.do") //지도 페이지로 이동
+	public ModelAndView GoMapPage(HttpSession session, ModelAndView mv) {
+		User loginUser = (User)session.getAttribute("loginUser");
+		mv.addObject("loginUser", loginUser).setViewName("Views/MapView");
+		return mv;
+	}
 	
 }
 
