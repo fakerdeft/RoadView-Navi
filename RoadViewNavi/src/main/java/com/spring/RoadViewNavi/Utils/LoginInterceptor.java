6eb3a026df96534f5898003dc9042b4a0470e7a3
fakerdeft <whakswp123@naver.com>
2023-01-06 00:29:08 +0900

    feat: 인터셉터 기능 수정

--- RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Utils/LoginInterceptor.java
+++ RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Utils/LoginInterceptor.java
@@ -11,30 +11,28 @@ import com.spring.RoadViewNavi.Model.VO.RoadViewNaviVO;
 public class LoginInterceptor implements HandlerInterceptor {
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-
-		// 로그인 되어있는지 아닌지 확인하는 작업
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		HttpSession session = request.getSession();
-
 		RoadViewNaviVO loginUser = (RoadViewNaviVO) session.getAttribute("loginUser");
-
-		if (loginUser != null) { // 로그인되어있는 정보가 있다면 Controller 요청 실행
-
-			if (loginUser.getUserId().equals("admin")) {
-				session.setAttribute("alertMsg", "관리자님 안녕하세요");
-			} else {
-//				session.setAttribute("alertMsg", loginUser.getUserName() + "님 안녕하세요.");
+		try {
+			// 로그인 되어있는지 아닌지 확인하는 작업
+			if (loginUser != null) {// 로그인되어있는 정보가 있다면 Controller 요청 실행
+				if (loginUser.getUserId().equals("admin")) {//관리자 계정일 경우
+					session.setAttribute("alertMsg", "관리자님 안녕하세요");
+				} else {
+					session.setAttribute("alertMsg", loginUser.getUserName() + "님 안녕하세요.");
+				}
+				return true;
 			}
-			return true;
-		} else {
-			// 로그인 되어 있지 않는 경우 - Controller 요청 처리 하지 않음
-			session.setAttribute("alertMsg", "로그인 후 가능한 서비스입니다.");
-			response.sendRedirect(request.getContextPath());
-
-			return false;
+			if (loginUser == null){// 로그인 되어 있지 않는 경우 - Controller 요청 처리 하지 않음
+				session.setAttribute("alertMsg", "로그인 후 가능한 서비스입니다.");
+				response.sendRedirect(request.getContextPath());
+				return false;
+			}
+		} catch(Exception e){
+			e.printStackTrace();
 		}
-		//test01
+		return true;
 
 	}
 
