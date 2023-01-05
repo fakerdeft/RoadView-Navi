6d1ed52aa94fbd983a935020cd7f942a7515fac0
fakerdeft <whakswp123@naver.com>
2023-01-06 01:25:52 +0900

    feat: user 객체 수정

--- RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Model/VO/User.java
+++ RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Model/VO/User.java
@@ -1,5 +1,7 @@
 package com.spring.RoadViewNavi.Model.VO;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor //기본생성자
 @AllArgsConstructor //모든필드 매개변수 생성자
 @Data
-public class RoadViewNaviVO {
+public class User {
 	private int userNo;
 	private String userId;
 	private String userPwd;
 	private String userName;
 	private String userPhone;
 	private String userEmail;
+	private Date enrollDate;
+	private Date updateDate;
+	private String enrollStatus;
 }
 
 
