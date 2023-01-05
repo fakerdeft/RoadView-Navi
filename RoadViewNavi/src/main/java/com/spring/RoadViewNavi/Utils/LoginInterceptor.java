c83082a36b8bca819fed1ca5db7cd42944abf834
fakerdeft <whakswp123@naver.com>
2023-01-06 01:23:52 +0900

    feat: 인터셉터 기능 수정

--- RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Utils/LoginInterceptor.java
+++ RoadViewNavi/src/main/java/com/spring/RoadViewNavi/Utils/LoginInterceptor.java
@@ -6,34 +6,48 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.spring.RoadViewNavi.Model.VO.RoadViewNaviVO;
+import com.spring.RoadViewNavi.Model.VO.User;
 
 public class LoginInterceptor implements HandlerInterceptor {
-
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		//비로그인 상태에서 url입력으로 접근 제한하는 기능
 		HttpSession session = request.getSession();
-		RoadViewNaviVO loginUser = (RoadViewNaviVO) session.getAttribute("loginUser");
-		try {
-			// 로그인 되어있는지 아닌지 확인하는 작업
-			if (loginUser != null) {// 로그인되어있는 정보가 있다면 Controller 요청 실행
-				if (loginUser.getUserId().equals("admin")) {//관리자 계정일 경우
-					session.setAttribute("alertMsg", "관리자님 안녕하세요");
-				} else {
+		User loginUser = (User) session.getAttribute("loginUser");
+		try {// 로그인 되어있는지 아닌지 확인하는 작업
+			if(loginUser != null){// 로그인 되어 있는 경우 - Controller 요청 실행
+				if(loginUser.getUserId().equals("admin")) {//관리자 계정일 경우
+					session.setAttribute("alertMsg", "관리자님 안녕하세요.");
+				}
+				if(!loginUser.getUserId().equals("admin")){//관리자 계정이 아닐 경우
 					session.setAttribute("alertMsg", loginUser.getUserName() + "님 안녕하세요.");
 				}
 				return true;
 			}
-			if (loginUser == null){// 로그인 되어 있지 않는 경우 - Controller 요청 처리 하지 않음
-				session.setAttribute("alertMsg", "로그인 후 가능한 서비스입니다.");
-				response.sendRedirect(request.getContextPath());
-				return false;
-			}
-		} catch(Exception e){
+		} catch(NullPointerException e){// 로그인 되어 있지 않는 경우 - Controller 요청 처리 하지 않음
 			e.printStackTrace();
+			session.setAttribute("alertMsg", "로그인 후 가능한 서비스입니다.");
+			response.sendRedirect(request.getContextPath());
 		}
-		return true;
-
+		return false;
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
